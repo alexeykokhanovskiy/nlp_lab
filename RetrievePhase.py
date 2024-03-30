7790142d3b4f27306e38ea6536ef3f232fe0f064
Alexey <alexey.kokhanovskiy@gmail.com>
2024-03-30 15:08:13 +0300

    retrieve phase commit

---
+++ RetrievePhase.py
@@ -0,0 +1,21 @@
+
+def retrieve_phase_GerberSaxton_mod (field_input, Niter, device):
+    # phase = torch.ones_like(field_input, dtype = torch.complex128)
+    field_input = field_input.to(device)
+    phase = torch.rand(field_input.size()[0],field_input.size()[1], dtype = torch.complex128)
+    phase = phase.to(device)
+    err = np.zeros(Niter)
+    AmpConstrait = torch.abs(field_input)
+    AmpConstrait = AmpConstrait.to(device)
+    FourierConstrait = torch.abs(torch.fft.fft2(field_input))
+    FourierConstrait = FourierConstrait.to(device)
+
+    for i in range(0,Niter):
+        phase[:] = AmpConstrait*torch.exp(1j*torch.angle(phase))
+        phase[:] = torch.fft.fft2(phase)
+        phase[:] = FourierConstrait*torch.exp(1j*torch.angle(phase))
+        phase[:] = torch.fft.ifft2(phase)
+        err[i] = torch.sqrt(torch.sum((torch.abs(field_input) - torch.abs(phase))**2))
+        # print(f'Iteration {i}: Error {err[i]}')
+    return torch.Tensor.cpu(phase), err
+
--- Wavefront.py
+++ Wavefront.py
@@ -109,28 +109,6 @@ class Wavefront():
     def get_k_space(self):
         return torch.abs(torch.Tensor.cpu(torch.fft.fftshift(wf.fourier)))
 
-def retrieve_phase_GerberSaxton_mod (field_input, Niter, device):
-    # phase = torch.ones_like(field_input, dtype = torch.complex128)
-    field_input = field_input.to(device)
-    phase = torch.rand(field_input.size()[0],field_input.size()[1], dtype = torch.complex128)
-    phase = phase.to(device)
-    err = np.zeros(Niter)
-    AmpConstrait = torch.abs(field_input)
-    AmpConstrait = AmpConstrait.to(device)
-    FourierConstrait = torch.abs(torch.fft.fft2(field_input))
-    FourierConstrait = FourierConstrait.to(device)
-
-    for i in range(0,Niter):
-        phase[:] = AmpConstrait*torch.exp(1j*torch.angle(phase))
-        phase[:] = torch.fft.fft2(phase)
-        phase[:] = FourierConstrait*torch.exp(1j*torch.angle(phase))
-        phase[:] = torch.fft.ifft2(phase)
-        err[i] = torch.sqrt(torch.sum((torch.abs(field_input) - torch.abs(phase))**2))
-        # print(f'Iteration {i}: Error {err[i]}')
-    return torch.Tensor.cpu(phase), err
-
-
-
 if __name__ == "__main__":
     wf = Wavefront(pixelNumber= 2**9, Lfield = 20e-3)
     wf.Gauss(beamFwhm=10e-3)
