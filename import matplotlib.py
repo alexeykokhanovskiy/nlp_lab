3e12e6da141ceb5b04bb1a4f2369c68d5d602e6a
Alexey <alexey.kokhanovskiy@gmail.com>
2024-04-20 15:24:27 +0300

    22

--- import matplotlib.py
+++ import matplotlib.py
@@ -1,9 +1,21 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from Wavefront import Wavefront
+from RetrievePhase import *
+import matplotlib.pyplot as plt 
 
-x = np.linspace(0,1,100)
-y = np.linspace(0,2,100)
-plt.plot(x)
-plt.plot(y)
 
-plt.show()
+wf = Wavefront()
+wf.Gauss(beamFwhm=1e-3)
+wf.Axicon(kp = 10000)
+wf.propagate_angular_spec(distance=10e-2)
+
+
+target_mask = wf.get_intensity() 
+wf.Gauss(beamFwhm=1e-3)
+input_intensity = wf.get_intensity()
+initial_phase = torch.randn(input_intensity.shape)
+spatial_field = torch.sqrt(input_intensity) * torch.exp(1j * initial_phase)
+
+uniform_amplitude = torch.sqrt(target_intensity)
+uniform_amplitude /= uniform_amplitude.max()
+# plt.imshow(wf.get_intensity())
+# plt.show()
